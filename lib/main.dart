752607a2feeb5001e2caaa1332bc566fca363980
Udit Khandelwal <uditkhandelwal1999@gmail.com>
2022-04-22 22:19:37 +0530

    file remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:interview_task/ui/homeNew.dart';
 import 'package:interview_task/utils/StringFile.dart';
 
 import 'ui/home_page.dart';
